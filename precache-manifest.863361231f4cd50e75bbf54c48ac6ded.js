ba41acd51441344b1975f6b912650d990f7b05ab
Bruno Costa <brunodejesusdacosta@gmail.com>
2020-10-25 20:40:24 -0300

    Updates

---
+++ precache-manifest.863361231f4cd50e75bbf54c48ac6ded.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c701c461e8c0b80d6d386f23e11ebca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f16b5e1bb781d2a62852",
+    "url": "/static/css/main.05326119.chunk.css"
+  },
+  {
+    "revision": "5f15dd860b673beea408",
+    "url": "/static/js/2.5105119d.chunk.js"
+  },
+  {
+    "revision": "bc01c4d38a86ca86b3401a17116d292e",
+    "url": "/static/js/2.5105119d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f16b5e1bb781d2a62852",
+    "url": "/static/js/main.ffac901d.chunk.js"
+  },
+  {
+    "revision": "648dfad5e0063bdc3a98",
+    "url": "/static/js/runtime-main.3ef713f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
